f2d510ce1d74bef0f9ac53f4cb0cc616277c839b
Tsvetelin Borisov <113609172+limonadamaster@users.noreply.github.com>
2024-01-13 10:58:47 +0200

    Delete service directory

--- service/CredententialService.java
+++
@@ -1,7 +0,0 @@
-package com.example.MyFitnessApp.service;
-
-import com.example.MyFitnessApp.model.Credentential;
-
-public interface CredententialService {
-    Credentential createCredentential(String username, String password);
-}
--- service/RegistrationService.java
+++
@@ -1,8 +0,0 @@
-package com.example.MyFitnessApp.service;
-
-import com.example.MyFitnessApp.model.Person;
-import com.example.MyFitnessApp.model.RegistationInformation;
-
-public interface RegistrationService {
-    public Person registerPersonWithCredentation(RegistationInformation registationInformation);
-}
