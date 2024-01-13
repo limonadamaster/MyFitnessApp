1051841a86455aeca24c4d479bf92a688e454edf
Tsvetelin Borisov <113609172+limonadamaster@users.noreply.github.com>
2024-01-13 10:58:54 +0200

    Delete repository directory

--- repository/CredententialRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.MyFitnessApp.repository;
-
-import com.example.MyFitnessApp.model.Credentential;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CredententialRepository extends JpaRepository<Credentential,Long> {
-}
