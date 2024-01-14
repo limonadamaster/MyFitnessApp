0059ab2b27ee3f431c8d45135b948b28f512001e
Your GitHub Username <your.email@example.com>
2024-01-14 21:53:35 +0200

    delete

--- MyFitnessApp/repository/FoodRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.MyFitnessApp.repository;
-
-import com.example.MyFitnessApp.model.Food;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FoodRepository extends JpaRepository<Food,Long> {
-    Food findByName(String name);
-}
